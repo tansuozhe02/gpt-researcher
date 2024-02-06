d658ea2dfb24e1983474c44e49a7e9434ce081f7
tansuozhe02 <tansuoAnalytics@gmail.com>
2024-02-06 10:02:57 +0000

    modeluid

--- gpt_researcher/memory/embeddings.py
+++ gpt_researcher/memory/embeddings.py
@@ -5,8 +5,8 @@ from xinference.client import Client
 
 class Memory:
     def __init__(self, **kwargs):
-        self._client = Client("http://localhost:9997")
-        self._model = self._client.get_model("MODEL_UID")
+        self._client = Client("http://127.0.0.1:9997")
+        self._model = self._client.get_model("mistral-instruct-v0.1")
 
     def create_embedding(self, text):
         return self._model.create_embedding(text)
