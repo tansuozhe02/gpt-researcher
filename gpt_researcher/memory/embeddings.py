2ac50a718b1382af0d9cde1f204e20beee09af9b
tansuozhe02 <tansuoAnalytics@gmail.com>
2024-02-06 12:26:51 +0000

    plural

--- gpt_researcher/memory/embeddings.py
+++ gpt_researcher/memory/embeddings.py
@@ -8,6 +8,6 @@ class Memory:
         self._client = Client("http://127.0.0.1:9997")
         self._model = self._client.get_model("mistral-instruct-v0.1")
 
-    def get_embedding(self, text):
+    def get_embeddings(self, text):
         return self._model.create_embedding(text)
 
