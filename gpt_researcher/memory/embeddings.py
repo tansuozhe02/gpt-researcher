51a032c8b6a8e2087da22c28f971644bb4aed1eb
tansuozhe02 <tansuoAnalytics@gmail.com>
2024-02-06 13:48:02 +0000

    embedding

--- gpt_researcher/memory/embeddings.py
+++ gpt_researcher/memory/embeddings.py
@@ -1,13 +1,19 @@
 from langchain.vectorstores import FAISS
-from xinference.client import Client
+from openai import Client
 
 
 
 class Memory:
     def __init__(self, **kwargs):
-        self._client = Client("http://127.0.0.1:9997")
-        self._model = self._client.get_model("mistral-instruct-v0.1")
-
+        self._client = openai.Client(
+            api_key="cannot be empty",
+            base_url="http://127.0.0.1:9997/v1"
+        )
     def get_embeddings(self, text):
-        return self._model.create_embedding(text)
+        result = self._client.embeddings.create(
+            model= "mistral-instruct-v0.1",
+            input=text
+        )
+        embedding=result["data"][0]["embedding"]
+        return embedding
 
