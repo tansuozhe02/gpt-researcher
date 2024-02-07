e33b9a5b9ab3e05f95c51451eec06143f46337c9
tansuozhe02 <tansuoAnalytics@gmail.com>
2024-02-07 11:30:09 +0000

    grammar

--- gpt_researcher/memory/embeddings.py
+++ gpt_researcher/memory/embeddings.py
@@ -7,6 +7,6 @@ class Memory:
         self._client = openai.Client(api_key="not empty", base_url="http://127.0.0.1:9997/v1")
 
     def get_embeddings(self, text):
-        embeddings = client.embeddings.create(model='mistral-instruct-v0.1', input=[text])
+        embeddings = self._client.embeddings.create(model='mistral-instruct-v0.1', input=[text])
         return embeddings
 
